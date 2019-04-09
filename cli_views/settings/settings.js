a3add1ebb1903660e4b650b99770004ee5a44da7
fgxaos <francois.garderes@student.ecp.fr>
2019-04-10 00:12:55 +0200

    Adds a settings menu

---
+++ cli_views/settings/settings.js
@@ -0,0 +1,54 @@
+/*
+TODO list: 
+    - Change username (if the username isn't already taken)
+    - Change password (check that the password is correct)
+    - Set/change an email address to send an email with the password to this address
+*/
+
+// Libraries
+const prompt = require("prompts");
+
+// Modules
+const globalViewMenu = require("../global_view.js");
+
+let interval;
+
+function launchSettingsMenu () {
+    (async function () {
+        const settingsMenu = 
+        {
+            type: "select",
+            name: "settingsMenu",
+            message: "What do you want to do?",
+            choices: [
+                { title: "Change username", value: "changeUsername", disabled: false }, 
+                { title: "Change password", value: "changePassword", disabled: false },
+                { title: "Set email address", value: "email", disabled: false},
+                { title: "Go back", value: "back", disabled: false}
+            ]
+        };
+        
+        const answer = await prompt(settingsMenu, {
+            onCancel: cleanup,
+            onSubmit: cleanup
+        });
+        switch (answer.settingsMenu) {
+            case "changeUsername":
+                console.log("Change username");
+                break;
+
+            case "changePassword": 
+                console.log("Change password");
+                break;
+            
+            case "email":
+                console.log("Set email address");
+                break;
+            
+            case "back":
+                console.log("Go back");
+                globalViewMenu();
+                break;
+        }
+    })
+}
