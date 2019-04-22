028d5e8cea52856974d82d2e34c26c33bf8c2b1e
fgxaos <francois.garderes@student.ecp.fr>
2019-04-22 04:08:27 +0200

    Starts monitors at the beginning

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const jwt = require("./users/_helpers/jwt");
 const errorHandler = require("./users/_helpers/errorHandler");
+const CLI = require("clui");
+const Spinner = CLI.Spinner;
+const userService = require("./users/userService");
+const Monitor = require("./compute_statistics/monitor");
+const alertRules = require("./compute_statistics/alert_rules");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -25,14 +30,43 @@ app.use(errorHandler);
 // Start server
 const port = process.env.NODE_ENV === "production" ? (process.env.PORT || 80) : 4000;
 const server = app.listen(port, function () {
-	console.log("Server listening on port " + port);
 });
 
-loginScreen();
+
+// Start the background monitoring process
+async function startMonitors () {
+	// Start loading spinner
+	let loadingSpinner = new Spinner("Loading: Starting monitoring", ["⣾","⣽","⣻","⢿","⡿","⣟","⣯","⣷"]);
+	loadingSpinner.start();
+
+	// Get all the monitors
+	let listMonitors = await userService.getAllMonitors();
+
+	// Start all of them to get data in real time
+	await listMonitors.map(monitor => {
+		let newMonitor = {
+			website: monitor.website_url,
+			title: monitor.website_url,
+			interval: monitor.checkTimeInterval,
+			name: monitor.name
+		};
+
+		global[monitor.name] = new Monitor(newMonitor);
+		alertRules(global[monitor.name]);
+
+	});
+
+	// Display loginScreen and stop loading spinner
+	loadingSpinner.stop();
+	loginScreen();
+}
+
+startMonitors();
+
 
 function closeServer () {
 	server.close(() => {
-		console.log('Process terminated');
+		console.log("Process terminated");
 	});
 }
 
